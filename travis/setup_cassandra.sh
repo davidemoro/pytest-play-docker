5d9cbb9f06a7fe0ce341637f0d63d8dcbc262b02
Davide Moro <davide.moro@gmail.com>
2019-02-11 17:25:28 +0100

    fix csql syntax error

--- travis/setup_cassandra.sh
+++ travis/setup_cassandra.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sleep 20
-cqlsh -e "create keyspace dev WITH REPLICATION = {'class' : 'SimpleStrategy',  'replication_factor' : 1};"
-cqlsh -e "create dev.table play(id int, title varchar); insert into dev.play (id, title) values (1, 'hello!');"
+cqlsh -e "create keyspace dev with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE dev; create table play(id int, title varchar); insert into play (id, title) values (1, 'hello!');"
