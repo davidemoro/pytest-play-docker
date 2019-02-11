6d7f917784cd53e6b588f0e6742c8fb92aeb120c
Davide Moro <davide.moro@gmail.com>
2019-02-11 22:56:03 +0100

    polling instead of sleep

--- travis/setup_cassandra.sh
+++ travis/setup_cassandra.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-sleep 20
+function cassandra_ready {
+    while ! travis_retry csqlsh -e "describe cluster;" ; do
+        echo "waiting for cassandra"
+        sleep 3
+    done
+}
+
+cassandra_ready
 cqlsh -e "create keyspace dev with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE dev; create table play(code int primary key, title varchar); insert into play (code, title) values (1, 'hello!');"
