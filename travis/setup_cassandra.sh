1a3bdfec4da24ba5660eeab5d850e5373830d45a
Davide Moro <davide.moro@gmail.com>
2019-02-11 17:28:26 +0100

    fix primary key cassandra

--- travis/setup_cassandra.sh
+++ travis/setup_cassandra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 20
-cqlsh -e "create keyspace dev with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE dev; create table play(id int, title varchar); insert into play (id, title) values (1, 'hello!');"
+cqlsh -e "create keyspace dev with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE dev; create table play(id int primary key, title varchar); insert into play (id, title) values (1, 'hello!');"
