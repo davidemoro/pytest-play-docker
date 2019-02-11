cb24ad4ccc47446cd5d425e6a36e5e9980804c3a
Davide Moro <davide.moro@gmail.com>
2019-02-11 17:58:42 +0100

    fix cassandra table name

--- travis/setup_cassandra.sh
+++ travis/setup_cassandra.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sleep 20
-cqlsh -e "create keyspace dev with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE dev; create table play(id int primary key, title varchar); insert into play (id, title) values (1, 'hello!');"
+cqlsh -e "create keyspace dev with replication = {'class': 'SimpleStrategy', 'replication_factor': 1}; USE dev; create table play(code int primary key, title varchar); insert into play (code, title) values (1, 'hello!');"
