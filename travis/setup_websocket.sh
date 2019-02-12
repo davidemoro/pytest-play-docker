f6ff0d4811394610f9706152cf330bfb5b450894
Davide Moro <davide.moro@gmail.com>
2019-02-12 13:43:38 +0100

    no more blocking

--- travis/setup_websocket.sh
+++ travis/setup_websocket.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/joewalnes/websocketd/releases/download/v0.3.0/websocketd-0.3.0-linux_amd64.zip
-unzip websocketd-0.3.0-linux_amd64.zip
-./websocketd --port=8080 ./echo_ws.sh &
+wget -P /tmp https://github.com/joewalnes/websocketd/releases/download/v0.3.0/websocketd-0.3.0-linux_amd64.zip
+unzip -o /tmp/websocketd-0.3.0-linux_amd64.zip -d /tmp
+./tmp/websocketd --port=8080 ./tmp/echo_ws.sh &
