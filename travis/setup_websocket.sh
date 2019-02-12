fc36e53056851a24c82fa9b0c7e126cb25f61fb3
Davide Moro <davide.moro@gmail.com>
2019-02-12 18:49:10 +0100

    fix path echo.sh

--- travis/setup_websocket.sh
+++ travis/setup_websocket.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -P /tmp https://github.com/joewalnes/websocketd/releases/download/v0.3.0/websocketd-0.3.0-linux_amd64.zip
 unzip -o /tmp/websocketd-0.3.0-linux_amd64.zip -d /tmp
-/tmp/websocketd --port=8080 ./echo_ws.sh &
+/tmp/websocketd --port=8080 $(pwd)/echo_ws.sh &
